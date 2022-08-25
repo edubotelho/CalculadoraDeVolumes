2721ff0ad55ab4e2b5d8fb848f582445a6ebfb78
Eduardo de Souza Botelho <edusouzabo@gmail.com>
2022-08-25 20:08:27 -0300

    addCalcCililndro
    atualização que coloca calculadora de cilindro

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ function calculate(){
 
     var lado = document.getElementById("valorB").value;
 
-    var altura = document.getElementById("valorh").value;
+    var altura = document.getElementById("valorAlt").value;
 
     var resposta = (area*lado)*altura
-    var res = document.getElementById('res')
+    let res = document.getElementById('res')
 
     res.innerHTML = `O volume é ${resposta}cm²`
     
@@ -19,6 +19,17 @@ function calculate(){
         alert('deu NÃO')
     }*/
 
+}
 
+function calculaCilindro(){
+    
+    var pi = 3.14;
+    var r = document.getElementById("valorR").value;
+    var h = document.getElementById("valorAlt").value;
 
+    var resCilindro = document.getElementById("resCilindro");
+
+    var result = (3.14*(r**2))*h;
+
+    resCilindro.innerHTML = `O volume é ${result}cm²`
 }
